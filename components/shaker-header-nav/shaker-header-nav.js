02dbe24718c089fe058e1d2e8f879808955e2a07
jshaker <jeremie.shaker@hotmail.com>
2016-06-27 07:54:16 -0400

    re-working css for header-nav

--- components/shaker-header-nav/shaker-header-nav.js
+++ components/shaker-header-nav/shaker-header-nav.js
@@ -3,14 +3,14 @@
 
     ShakerHeadernavProto.createdCallback = function(){
         var shadow = this.createShadowRoot();
-        var importDocument = document.querySelector('#shaker-headernav').import;
-        var template = importDocument.querySelector('#shaker-headernav');
+        var importDocument = document.querySelector('#shaker-header-nav').import;
+        var template = importDocument.querySelector('#shaker-header-nav');
         var templateContent = document.importNode(template.content, true);
         shadow.appendChild(templateContent);
     };
 
 
-    document.registerElement('shaker-headernav', {
+    document.registerElement('shaker-header-nav', {
         prototype: ShakerHeadernavProto
     });
 })();
--- components/shaker-title/shaker-title.js
+++ components/shaker-title/shaker-title.js
@@ -4,12 +4,8 @@
     ShakerTitleProto.createdCallback = function(){
         var shadow = this.createShadowRoot();
         var importDocument = document.querySelector('#shaker-title').import;
-        var importStyling = document.querySelector('#shaker-sidenav-title').import;
         var template = importDocument.querySelector('#shaker-title');
-        var style = importStyling.querySelector('#shaker-sidenav-title');
         var templateContent = document.importNode(template.content, true);
-        var styleContent = document.importNode(style.content,true);
-        templateContent.appendChild(styleContent);
         var span = templateContent.querySelector('span');
         shadow.appendChild(templateContent);
     };
