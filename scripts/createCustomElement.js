e5e8ff641b569ce204246dbb06953b9667f9800f
jshaker <jeremie.shaker@hotmail.com>
2016-07-04 23:25:08 -0400

    deprecated functionality to create x-component without transferability of attributes. I find it counter-intuitive to have components have attributes on the light DOM without any effect on the shadow DOM, making the light DOM attributes useless

--- scripts/createCustomElement.js
+++ scripts/createCustomElement.js
@@ -1,74 +1,54 @@
 var createCustomElement = function(elementName, elementSelector, elementExtends){
     var proto = Object.create(HTMLElement.prototype);
 
+    if(elementName == null){
+        throw new Error("Invalid element name: " + elementName);
+    }
 
-    if(elementSelector){
-        proto.attributeChangedCallback = function(attrName, oldVal, newVal){
-            this.shadowRoot.querySelector(elementSelector).setAttribute(attrName,newVal);
+    if(elementSelector == null) {
+        throw new Error("Invalid element selector: " + elementSelector);
+    }
+    proto.attributeChangedCallback = function(attrName, oldVal, newVal){
+        this.shadowRoot.querySelector(elementSelector).setAttribute(attrName,newVal);
+    };
+    if(elementExtends){
+        proto.createdCallback = function(){
+            var shadow = this.createShadowRoot();
+            var importDocument = document.querySelector('#'+elementExtends).import;
+            var importStyling = document.querySelector('#'+elementName).import;
+            var template = importDocument.querySelector('#'+elementExtends);
+            var style = importStyling.querySelector('#'+elementName);
+            var templateContent = document.importNode(template.content, true);
+            var styleContent = document.importNode(style.content,true);
+            templateContent.appendChild(styleContent);
+            var element = templateContent.querySelector(elementSelector);
+
+            for(var key in this.attributes){
+                if(!this.attributes[key] || this.attributes[key].nodeName == null){
+                    continue;
+                }
+                var attribute = this.attributes[key];
+                element.setAttribute(attribute.nodeName,attribute.nodeValue);
+            }
+            shadow.appendChild(templateContent);
         };
-        if(elementExtends){
-            proto.createdCallback = function(){
-                var shadow = this.createShadowRoot();
-                var importDocument = document.querySelector('#'+elementExtends).import;
-                var importStyling = document.querySelector('#'+elementName).import;
-                var template = importDocument.querySelector('#'+elementExtends);
-                var style = importStyling.querySelector('#'+elementName);
-                var templateContent = document.importNode(template.content, true);
-                var styleContent = document.importNode(style.content,true);
-                templateContent.appendChild(styleContent);
-                var element = templateContent.querySelector(elementSelector);
-
-                for(var key in this.attributes){
-                    if(!this.attributes[key] || this.attributes[key].nodeName === undefined){
-                        continue;
-                    }
-                    var attribute = this.attributes[key];
-                    element.setAttribute(attribute.nodeName,attribute.nodeValue);
-                }
-                shadow.appendChild(templateContent);
-            };
-        }
-        else{
-            proto.createdCallback = function(){
-                var shadow = this.createShadowRoot();
-                var importDocument = document.querySelector('#'+elementName).import;
-                var template = importDocument.querySelector('#'+elementName);
-                var templateContent = document.importNode(template.content, true);
-                var element = templateContent.querySelector(elementSelector);
-                for(var key in this.attributes){
-                    if(!this.attributes[key] || this.attributes[key].nodeName === undefined){
-                        continue;
-                    }
-                    var attribute = this.attributes[key];
-                    element.setAttribute(attribute.nodeName,attribute.nodeValue);
-                }
-                shadow.appendChild(templateContent);
-            };
-        }
     }
     else{
-        if(elementExtends){
-            proto.createdCallback = function(){
-                var shadow = this.createShadowRoot();
-                var importDocument = document.querySelector('#'+elementExtends).import;
-                var importStyling = document.querySelector('#'+elementName).import;
-                var template = importDocument.querySelector('#'+elementExtends);
-                var style = importStyling.querySelector('#'+elementName);
-                var templateContent = document.importNode(template.content, true);
-                var styleContent = document.importNode(style.content,true);
-                templateContent.appendChild(styleContent);
-                shadow.appendChild(templateContent);
-            };
-        }
-        else{
-            proto.createdCallback = function(){
-                var shadow = this.createShadowRoot();
-                var importDocument = document.querySelector('#'+elementName).import;
-                var template = importDocument.querySelector('#'+elementName);
-                var templateContent = document.importNode(template.content, true);
-                shadow.appendChild(templateContent);
-            };
-        }
+        proto.createdCallback = function(){
+            var shadow = this.createShadowRoot();
+            var importDocument = document.querySelector('#'+elementName).import;
+            var template = importDocument.querySelector('#'+elementName);
+            var templateContent = document.importNode(template.content, true);
+            var element = templateContent.querySelector(elementSelector);
+            for(var key in this.attributes){
+                if(!this.attributes[key] || this.attributes[key].nodeName == null){
+                    continue;
+                }
+                var attribute = this.attributes[key];
+                element.setAttribute(attribute.nodeName,attribute.nodeValue);
+            }
+            shadow.appendChild(templateContent);
+        };
     }
 
     return document.registerElement(elementName, {
